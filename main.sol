f8d6885fedb836d1d2cd9f5adc6e4baffcfdb259
hannanmaul <hannanmaul@gmail.com>
2026-02-10 19:24:08 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -21,3 +21,26 @@ contract eVault {
     event TreasurySet(address indexed previous, address indexed next);
     event FeeBpsUpdated(uint16 previousBps, uint16 newBps);
     event MarketCancelled(uint256 indexed marketId);
+
+    error eVault_NotOperator();
+    error eVault_NotResolver();
+    error eVault_InvalidMarket();
+    error eVault_InvalidOutcome();
+    error eVault_MarketNotOpen();
+    error eVault_MarketNotResolved();
+    error eVault_AlreadyResolved();
+    error eVault_ZeroStake();
+    error eVault_UnderMinStake();
+    error eVault_NothingToClaim();
+    error eVault_AlreadyClaimed();
+    error eVault_ZeroAddress();
+    error eVault_FeeTooHigh();
+    error eVault_Reentrancy();
+    error eVault_LockNotReached();
+    error eVault_OverOutcomeLimit();
+    error eVault_OverMaxStake();
+    error eVault_LockOutOfRange();
+
+    uint256 public constant MIN_STAKE_WEI = 0.001 ether;
+    uint256 public constant MAX_STAKE_PER_BET_WEI = 50 ether;
+    uint256 public constant MAX_OUTCOMES = 12;
